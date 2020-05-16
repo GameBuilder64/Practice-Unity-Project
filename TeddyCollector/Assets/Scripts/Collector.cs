a979591e5bf5a7476dac3bb9703a46aabc1fa207
GameBuilder64 <caslacetech@gmail.com>
2020-05-15 19:04:52 -0700

    Can Spawn Pickup now

---
+++ TeddyCollector/Assets/Scripts/Collector.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+    //SortedList<Target> targets = new SortedList<Target>();
+    Target targetPickup = null;
+
+
+    const float BaseImpulseForceMagnitude = 2.0f;
+    const float ImpulseForceIncrement = 0.3f;
+
+    // saved for efficiency
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // center collector in sccreen
+        Vector3 position = transform.position;
+        position.x = 0;
+        position.y = 0;
+        position.z = 0;
+        transform.position = position;
+
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject == targetPickup.GameObject)
+        {
+            // add remove collected pickup from list of targets and game
+            // go to next target if there is one
+
+        }
+    }
+    
+    void SetTarget(GameObject pickup)
+    {
+        targetPickup.gameObject = pickup;
+        GoToTargetPickup();
+    }
+
+    void GoToTargetPickup()
+    {
+        // calculate direction to target pickup
+        Vector2 direction = new Vector2(
+            targetPickup.gameObject.transform.position.x - transform.position.x,
+            targetPickup.gameObject.transform.position.y - transform.position.y);
+        direction.Normalize();
+        rb2d.velocity = Vector2.zero;
+        rb2d.AddForce(direction * BaseImpulseForceMagnitude, ForceMode2D.Impulse);
+    }
+
+
+}
---
+++ TeddyCollector/Assets/Scripts/Target.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    #region
+
+    public GameObject gameObject;
+    float distance;
+
+    #endregion
+
+    public Target(GameObject gameObject, Vector3 position)
+    {
+        this.gameObject = gameObject;
+        UpdateDistance(position);
+    }
+
+    public GameObject GameObject
+    {
+        get { return gameObject; }
+    }
+
+    public float Distance
+    {
+        get { return distance; }
+    }
+
+
+    public void UpdateDistance(Vector3 postion)
+    {
+        distance = Vector3.Distance(gameObject.transform.position, postion);
+    }
+
+    /// <summary>
+    /// Compares the current instance with another object of the same type
+    /// and returns an integer that indicates whether the current instance
+    /// precedes, follows, or occurs in the same position in the sort order
+    /// as the other object.
+    /// </summary>
+    public int CompareTo(object obj)
+    {
+
+        return 0;
+    }
+
+    //Coverts the target to a string
+    public override string ToString()
+    {
+        return "[Target: Distance = " + distance + "]";
+    }
+
+
+
+}
